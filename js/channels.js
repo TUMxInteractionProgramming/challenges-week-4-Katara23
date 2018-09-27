d1d564a6b14197432e4cd263ce18eb7f73930456
Christelle <Katara23@users.noreply.github.com>
2018-09-26 21:59:06 -0700

    Added Font Awesome icons and channel objects

---
+++ js/channels.js
@@ -0,0 +1,68 @@
+/*
+var channel = {
+    name: "",
+    createdOn:
+        function() {
+            let d = new Date();
+            let options = {
+                weekday: "short",
+                month: "short",
+                day: "numeric",
+                //year: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                hour12: false
+            };
+            return d.toLocaleString("en-US", options);
+        },
+    createdBy: "",
+    starred: false,
+    expiresIn: 0,
+    messageCount: 0
+};
+*/
+
+var yummy = {
+    name: "#Yummy",
+    createdOn: new Date("April 1, 2016"),
+    createdBy: "minus.plus.yummy",
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var sevenContinents = {
+    name: "#SevenContinents",
+    createdOn: new Date("April 1, 2016"),
+    createdBy: "cheeses.yard.applies",
+    starred: true,
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var killerApp = {
+    name: "#KillerApp",
+    createdOn: new Date("April 1, 2016"),
+    createdBy: "upgrading.never.helps",
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var firstPersonOnMars = {
+    name: "#FirstPersonOnMars",
+    createdOn: new Date("April 1, 2016"),
+    createdBy: "upgrading.never.helps",
+    starred: true,
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var octoberfest = {
+    name: "#Octoberfest",
+    createdOn: new Date("April 1, 2016"),
+    createdBy: "upgrading.never.helps",
+    starred: false,
+    expiresIn: 100,
+    messageCount: 999
+};
--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
 /* #6 start the #external #action and say hello */
 console.log("App is alive");
 
+var currentChannel;
+
+var currentLocation = {
+    longitude: 34.02055,
+    latitude: -118.28547,
+    what3words: "marked.future.chose"
+};
+
 /**
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
 function switchChannel(channelName) {
     //Log the channel switch
-    console.log("Tuning in to channel", channelName);
+    console.log("Tuning in to channel", channelName.name);
+
+    currentChannel = channelName;
+    //console.log(currentChannel);
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = channelName.name;
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/' + channelName.createdBy +'" target="_blank"><strong>' + channelName.createdBy + '</strong></a>';
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
-
+    //Original code
+    //$('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    $('#channels li:contains(' + channelName.name + ')').addClass('selected');
 }
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+    $('#channel-star').toggleClass('far fas');
 }
 
 /**
